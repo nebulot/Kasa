8ad5ade7a62175b5e1de7a7bb9867cc615555fa4
Torkneb <87141819+nebulot@users.noreply.github.com>
2025-02-19 12:00:18 +0000

    ajout du fichier sur github

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import "./navbar.css";
+import "./Navbar.css";
 
 function Navbar() {
   return (
     <nav className="navbar">
-        <div className="navbar__logo">
-            <img src="./LOGO.svg" alt="logo"/>
-        </div>
-        <div>Accueil</div>
-        <div>A propos</div>
+      <div className="navbar__logo">
+        <img src="./LOGO.svg" alt="logo" />
+      </div>
+      <div>Accueil</div>
+      <div>A propos</div>
     </nav>
   )
 }
