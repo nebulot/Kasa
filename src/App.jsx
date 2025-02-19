0798be68625f3c5e2404d1d66fafa8dd649497f5
Torkneb <87141819+nebulot@users.noreply.github.com>
2025-02-19 12:56:05 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "./Components/Navbar.jsx";
+import Banner from "./Components/Banner.jsx";
+import "./App.css";
+
+
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Banner/>
+      
+    </div>
+  );
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+        <div className="navbar__logo">
+            <img src="./LOGO.svg" alt="logo"/>
+        </div>
+        <div>Accueil</div>
+        <div>A propos</div>
+    </nav>
+  )
+}
+
+export default Navbar
