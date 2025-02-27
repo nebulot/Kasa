f25649292faff0176be103bca9234c9b99f98d1b
Torkneb <87141819+nebulot@users.noreply.github.com>
2025-02-27 16:15:54 +0000

    fetch , lecture des informations ajout props

--- src/Components/ApartmentCard.jsx
+++ src/Components/ApartmentCard.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import "./ApartmentGrid.scss";
 
-function ApartmentCard() {
+function ApartmentCard(props) {
     return (
-        <Link to="/flat">
+        <Link to="/flat"
+            state={{
+                apartmentId: props.id
+            }}
+        >
             <div className="apartment">
-                <div className="apartment__subtitle">Titre de la location</div>
+                <img src={props.imageUrl} alt="" />
+                <div className="apartment__subtitle">{props.title}</div>
             </div>
         </Link>
     );
--- src/Components/ApartmentGrid.jsx
+++ src/Components/ApartmentGrid.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import "./ApartmentGrid.scss";
 import ApartmentCard from "./ApartmentCard";
+import { useApartments } from "./../hooks/useApartments.jsx";
+
+//fetch("db.json").then((res) => res.json()).then(console.log);
 
 function ApartmentGrid() {
+    const apartments = useApartments();
     return (
         <div className="grid">
-            <ApartmentCard />
-            <ApartmentCard />
-            <ApartmentCard />
-            <ApartmentCard />
+            {apartments.map((apartment) => (
+                <ApartmentCard
+                    title={apartment.title}
+                    imageUrl={apartment.cover}
+                    id={apartment.id}
+                    key={apartment.id}
+                />
+            ))}
         </div>
-    )
+    );
 }
 
 export default ApartmentGrid
