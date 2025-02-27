4424a16bad1ea4bcf5390a660214733c2ddffa94
Torkneb <87141819+nebulot@users.noreply.github.com>
2025-02-27 19:44:28 +0000

    carroussel image apartImgBanner et useApartment simple

--- src/Components/ApartImgBanner.jsx
+++ src/Components/ApartImgBanner.jsx
@@ -1,11 +1,58 @@
-import React from 'react';
-import "../Components/ApartImgBanner.scss";
+import React, { useState } from "react";
+import "./ApartImgBanner.scss";
 
-export function ApartImgBanner() {
+export function ApartImgBanner(props) {
+    const pictures = props.pictures;
+
+    const [currentPicture, setCurrentPicture] = useState(0);
+
+    const getClassName = (i) => {
+        if (i === currentPicture) return "show";
+        return "";
+    };
+
+    const moveToNext = () => {
+        setCurrentPicture((currentPicture + 1) % pictures.length);
+    };
+
+    const moveToPrevious = () => {
+        const newCurrentPicture = currentPicture - 1;
+        if (newCurrentPicture < 0) {
+            setCurrentPicture(pictures.length - 1);
+            return;
+        }
+        setCurrentPicture(currentPicture - 1);
+    };
+
+    const arePicturesAvailable = () => {
+        return pictures && pictures.length > 0;
+    };
+
+    const getCarouselOrDefaultImage = () => {
+        if (!arePicturesAvailable()) {
+            return <img src="https://picsum.photos/800" className="show" alt="" />;
+        }
+        return pictures.map((pic, i) => (
+            <img key={pic} src={pic} alt="" className={getClassName(i)}></img>
+        ));
+    };
+
     return (
         <div className="banner__img">
-            <img src="https://picsum.photos/seed/picsum/800/400" alt="" />
+            <div className="banner__img__container">{getCarouselOrDefaultImage()}</div>
+            {arePicturesAvailable() && (
+                <>
+                    <button className="btn btn-previous" onClick={moveToPrevious}>
+                        <i className="fas fa-chevron-left"></i>
+                    </button>
+                    <span className="slide-counter">
+                        {currentPicture + 1} / {pictures.length}
+                    </span>
+                    <button className="btn btn-next" onClick={moveToNext}>
+                        <i className="fas fa-chevron-right"></i>
+                    </button>
+                </>
+            )}
         </div>
-    )
+    );
 }
-
--- src/Pages/ApartmentPage.jsx
+++ src/Pages/ApartmentPage.jsx
@@ -1,22 +1,29 @@
-import React from "react";
-import "./ApartmentPage.scss";
+import React, { useEffect, useState } from "react";
 import { ApartmentDescription } from "../Components/ApartmentDescription";
 import { ApartImgBanner } from "../Components/ApartImgBanner";
 import { ApartmentHeader } from "../Components/ApartmentHeader";
+import { useApartment } from "../hooks/useApartment";
+import "./ApartmentPage.scss";
+
 
 function ApartmentPage() {
+    const flat = useApartment();
 
+    if (flat == null) return <div>Loading...</div>;
     return (
         <div className="apartment__page">
-            <ApartImgBanner />
-            <ApartmentHeader />
-
+            <ApartImgBanner pictures={flat.pictures} />
+            <ApartmentHeader flat={flat} />
             <div className="apartment__description__area">
-                <ApartmentDescription />
-                <ApartmentDescription />
+                <ApartmentDescription title="Description" content={flat.description} />
+                <ApartmentDescription
+                    title="Equipements"
+                    content={flat.equipments.map((eq, i) => (
+                        <li key={i}>{eq}</li>
+                    ))}
+                />
             </div>
-        </div >
+        </div>
     );
 }
-
 export default ApartmentPage;
