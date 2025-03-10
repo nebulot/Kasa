b2dab4631d2be77919efda486116ecdaccd89909
Torkneb <87141819+nebulot@users.noreply.github.com>
2025-03-10 17:48:12 +0100

    Update vite.config.js
    vite config nom repository

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Nebulotelodie_Kasa/", // ⚠️ le nom de mon repo GitHub Pages
 })
